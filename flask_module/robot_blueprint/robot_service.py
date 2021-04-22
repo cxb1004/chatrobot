63fff52a3860b63a6318fec079f4cfd31b7575e1
chengxb <chengxb@53kf.com>
2021-04-22 20:31:45 +0800

    机器人主框架开发

--- flask_module/robot_blueprint/robot_service.py
+++ flask_module/robot_blueprint/robot_service.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_module.log_service import ServiceLog
 from flask_module.result_json import *
 from flask_module.robot_blueprint import robot_blueprint
+from flask_module.robot_blueprint.Model.rbt_robot import Robot
 from flask_module.utils import *
 
 ROBOT_LIST = {}
@@ -10,72 +11,167 @@ ROBOT_LIST = {}
 slog = ServiceLog()
 
 
-@robot_blueprint.route('/service/answer', methods=['POST'])
-def answer():
-    """
-    机器人回答问题
-    1、接收参数,如果参数缺失，就返回错误
-    - rbt_id 机器人ID
-    - question 访客的问题
-    2、根据rbt_id，在列表中确认机器人是否存在
-    2.1 如果不存在，就新建机器人实例
-    2.1.1 新建成功，就把机器人实例加到ROBOT_LIST里面，用rbt_id作为主键
-    2.1.2 新建失败，返回异常信息
-    3、从ROBOT_LIST里面提取机器人实例
-
-
-
-
-
-    2.1 如果存在，就在ROBOT_LIST直接提取机器人对象Robot
-    2.2 如果不存在，就尝试加载机器人对象，并放入ROBOT_LIST
-    2.2.1 如果加载成功，没有问题
-    2.2.2 如果加载失败，返回错误信息
-    3、从ROBOT_LIST里面获得ROBOT对象，从中获得行业机器人ID
-    3.1 如果没有行业机器人就忽略
-    3.2 如果有行业机器人，就加载行业机器人
-    3.2.1 如果行业机器人加载成功，就用行业机器人进行问答
-    3.2.2 如果行业机器人加载失败，就忽略
-    4、使用
-
-
-    :return:
-    """
-    rbt_id = request.form.get('rbt_id', type=str)
-    question = request.form.get("question",type=str)
-    if isNullOrBlank(rbt_id) or isNullOrBlank(question):
-        return return_fail("参数缺失！")
-    if rbt_id not in ROBOT_LIST.keys():
-        pass
-    # 从列表中获取机器人
-    robot = ROBOT_LIST.get(rbt_id)
-
-    # 如果有行业机器人，那么就要调用行业机器人
-    if robot.industry_rbt_id is not None:
-        pass
-
-
 def load_robot(rbt_id):
     """
-    1、检查机器人是否已经存在，如果存在，无需导入
-    2、获取机器人的运行数据
-    - 机器人ID
-    - 机器人的语料库（use_model=0）
-    - 相似度阀值
-    - 机器人的模型
-    - 模型判断阀值
-    - 行业机器人ID
-    - 机器人的自动卸载时间
+    加载机器人操作：
+    1、检查机器人是否已经存在于ROBOT_LIST，如果已经存在，结束函数
+    2、获得机器人的基本信息：
+    - 机器人ID （传入）
+    - 相似度阀值，读取这个机器人配置，如果没有就读取配置文件的基础配置
+    - 知识库，允许为空
+    - 行业机器人ID：根据机器人所属行业来查询
+    - 模型：用于模型判断
+    - 机器人自动卸载时间
+    3、组件机器人实例，并以rbt_id为主键，放入ROBOT_LIST字典
+    4、如果行业机器人ID不为空，就载入行业机器人
+    如果出错就报警，但是不影响当前机器人的载入
+
     :param rbt_id:
     :return:
     """
+    # 1、检查机器人是否已经存在于ROBOT_LIST，如果已经存在，结束函数
     if rbt_id in ROBOT_LIST.keys():
         slog.warn("机器人已经加载，无需操作！")
 
+    # 2、获得机器人的基本信息,组件机器人实例
+    cRobot = Robot()
+    try:
+        cRobot.assemble(rbt_id=rbt_id)
+    except Exception as ex:
+        errMsg = "组建机器人{}失败，请联系系统管理员！".format(rbt_id)
+        raise Exception(errMsg)
+
+    # 3、rbt_id为主键，放入ROBOT_LIST字典
+    ROBOT_LIST[rbt_id] = cRobot
+    slog.info("企业机器人{}载入成功".format(rbt_id))
+
+    # 4、如果行业机器人ID不为空，就载入行业机器人
+    industry_robot_id = cRobot.getIndustryRobotID()
+    if industry_robot_id is not None and industry_robot_id not in ROBOT_LIST.keys():
+        load_industry_robot(cRobot.getIndustryRobotID())
+
+
+def load_industry_robot(industry_robot_id):
+    # TODO 由于暂时没有行业机器人
     pass
 
 
+def mergeAnswer(c_answers, i_answers):
+    """
+    合并行业模型和企业模型的判断
+    返回的数据可以根据业务随时进行调整
+    目前的合并逻辑如下：
+    1、把企业机器人答案/行业机器人答案进行合并
+    2、按照答案的simValue进行降序排列
+    3、获得排序前五大条目，组成最终列表数据，进行返回
+    :param c_answers:
+    :param i_answers:
+    :return:
+    """
 
+    c_answers.extend(i_answers)
+    c_answers.sort(key=lambda i: i['sim_val'])
+
+    c_answers.sort(key=lambda i: i['sim_val'], reverse=True)
+
+    # TODO 这里的5是最终返回给前端的数据，重构的时候写到配置文件里
+    if c_answers.__len__() > 5:
+        cnt = 5
+    else:
+        cnt = c_answers.__len__()
+    return c_answers[:cnt]
+
+
+@robot_blueprint.route('/service/answer', methods=['POST'])
+def inf_answer():
+    """
+        机器人回答问题
+        1、接收参数,如果参数缺失，就返回错误
+        - rbt_id 机器人ID
+        - question 访客的问题
+        2、根据rbt_id，在列表中确认机器人是否存在
+        2.1 如果不存在，就load机器人实例
+        2.1.1 load成功，就把机器人实例加到ROBOT_LIST里面，用rbt_id作为主键
+        2.1.2 load失败，返回异常信息
+        3、从ROBOT_LIST里面提取企业机器人实例cRobot
+        4、使用企业机器人实例cRobot进行answer回答，分别获得前n个知识库匹配的数据，以及模型判断的结果
+        5、获得企业机器人cRobot的所属行业机器人ID
+        通过企业机器人所属行业，查询是否存在行业机器人（这个逻辑在创建企业机器人的时候做）
+        5.1 如果企业机器人cRobot没有配置行业，跳过
+        5.2 如果企业机器人cRobot所属行业（有行业ID），还没有配置行业机器人，跳过
+        5.3 如果企业机器人cRobot所属行业有行业机器人，就获取ID
+        6、根据行业机器人ID，在ROBOT_LIST里面查询是否已加载行业机器人
+        6.1 如果已经加载，直接获得行业机器人实例iRobot
+        6.2 如果尚未加载，根据行业机器人ID进行加载
+        如果加载过程出错，发出警告，跳过 （不影响返回企业机器人答案）
+        7、使用行业机器人iRobot进行回答
+        8、根据question_id,整合两部分的答案，返回数据
+        - question_id:
+        - idx_value:
+        - type: k/m   k代表知识库，m代表模型判断
+        - tag：i/c    i代表行业机器人/通用机器人   c代表企业机器人
+        9、在调用answer接口之后，更新机器人的卸载时间(包括行业机器人和企业机器人)
+    :return:
+        - question_id:
+        - idx_value:
+        - type: k/m   k代表知识库，m代表模型判断
+        - tag：i/c    i代表行业机器人/通用机器人   c代表企业机器人
+    """
+    # 1、接收参数, 如果参数缺失，就返回错误
+    rbt_id = request.form.get('rbt_id', type=str)
+    question = request.form.get("question", type=str)
+    if isNullOrBlank(rbt_id) or isNullOrBlank(question):
+        return return_fail("参数缺失！")
+
+    # 2、根据rbt_id，在列表中确认机器人是否存在
+    if rbt_id not in ROBOT_LIST.keys():
+        try:
+            # 载入机器人
+            load_robot(rbt_id)
+        except Exception as ex:
+            errMsg = "企业机器人{}载入失败，请联系系统管理员！".format(rbt_id)
+            slog.error_ex(errMsg)
+            return return_fail(errMsg)
+    # 3、从ROBOT_LIST里面提取企业机器人实例cRobot
+    cRobot = ROBOT_LIST.get(rbt_id)
+    if cRobot is None:
+        errMsg = "企业机器人{}调用失败，请联系系统管理员！".format(rbt_id)
+        return return_fail(errMsg)
+
+    # 4、使用企业机器人实例cRobot进行answer回答，分别获得前n个知识库匹配的数据，以及模型判断的结果
+    c_answers = cRobot.answer(question)
+
+    # 5、获得企业机器人cRobot的所属行业机器人ID
+    industry_robot_id = cRobot.getIndustryRobotID()
+    # 6、根据行业机器人ID，在ROBOT_LIST里面查询是否已加载行业机器人
+    if industry_robot_id is not None:
+        # 6.2 如果尚未加载，根据行业机器人ID进行加载
+        if industry_robot_id not in ROBOT_LIST.keys():
+            try:
+                load_industry_robot(industry_robot_id)
+            except Exception as ex:
+                # 如果加载过程出错，发出警告，跳过 （不影响返回企业机器人答案）
+                warnMsg = "企业机器人{}载入失败，请联系系统管理员！".format(rbt_id)
+                slog.warn(warnMsg)
+
+        # 6.1 如果已经加载，直接获得行业机器人实例iRobot
+        iRobot = ROBOT_LIST.get(industry_robot_id)
+        if iRobot is None:
+            # 如果加载过程出错，发出警告，跳过 （不影响返回企业机器人答案）
+            slog.warn("行业机器人调用失败，请联系管理员！".format(industry_robot_id))
+    else:
+        # 如果企业机器人本身没有行业机器人配置（企业机器人没有行业 / 有行业但是没有生成行业机器人）
+        # 设置行业机器人为空，不影响回答操作
+        iRobot = None
+
+    # 7、使用行业机器人iRobot进行回答
+    if iRobot is not None:
+        i_answers = iRobot.answer(question)
+
+    # 根据question_id,整合两部分的答案，返回数据
+    rtn_answer = mergeAnswer(c_answers, i_answers)
+
+    return return_success(rtn_answer)
 
 
 @robot_blueprint.route('/service/loadRobot', methods=['POST'])
