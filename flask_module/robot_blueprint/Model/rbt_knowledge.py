3705b9d0d6b9642eebd71e5c499c18ebae838940
chengxb <chengxb@53kf.com>
2021-04-23 16:50:31 +0800

    完成机器人的组装功能

--- flask_module/robot_blueprint/Model/rbt_knowledge.py
+++ flask_module/robot_blueprint/Model/rbt_knowledge.py
@@ -1,5 +1,4 @@
 from flask_module.db_utils import *
-from flask_module.robot_blueprint.constants import RobotConstants
 
 
 def getKnowledgeDataForClusterAnalysis(app=None, sess=None, rbt_id=None):
@@ -37,23 +36,31 @@ def getKnowledgeDataForAnswer(app=None, sess=None, rbt_id=None):
     :param app:
     :return:
     """
-    sql = '''select id, parent_id,question FROM ai_chatrobot.rbt_knowledge where rbt_id=:rbt_id and use_model=:use_model order by parent_id asc, id asc'''
-    params = {'rbt_id': rbt_id, 'use_model': RobotConstants.KNOWLEDGE_USE_MODEL_NO_PASS}
+    # , 'use_model': RobotConstants.KNOWLEDGE_USE_MODEL_NO_PASS
+    sql = '''select id, parent_id,question,use_model FROM ai_chatrobot.rbt_knowledge where rbt_id=:rbt_id order by parent_id asc, id asc'''
+    params = {'rbt_id': rbt_id}
     queryData = queryBySQL(app=app, sess=sess, sql=sql, params=params)
     # 知识库的结构：  文本：id   id有可能重复，如果文本有重复，后面的数据替换前面的数据
-    knowledge_lib = {}
+    # full是整个知识库，用于模型判断结果的评分； corpus是使用
+    full = {}
+    corpus = {}
     for id_pid_q in queryData:
         id = id_pid_q.get('id')
         pid = id_pid_q.get('parent_id')
         q = id_pid_q.get('question')
+        use_model = id_pid_q.get('use_model')
 
         if pid == '0' or pid is None:
             # 如果是标准问题, 则添加<question, id>
-            knowledge_lib = {q, id}
+            if use_model == 0:
+                corpus = {q, id}
+            full = {q, id}
         else:
             # 如果是相似问题, 则添加<question, parent_id>
-            knowledge_lib = {q, pid}
-    return knowledge_lib
+            if use_model == 0:
+                corpus = {q, id}
+            full = {q, id}
+    return corpus, full
 
 
 8
--- flask_module/robot_blueprint/Model/rbt_robot.py
+++ flask_module/robot_blueprint/Model/rbt_robot.py
@@ -1,4 +1,13 @@
+from flask import current_app
+
+from flask_module.robot_blueprint.Model.rbt_knowledge import *
+from flask_module.robot_blueprint.constants import RobotConstants
 from flask_module.utils import *
+
+
+def assemble_model(self, rbt_id):
+    # TODO 根据rbt_id载入模型
+    pass
 
 
 class Robot:
@@ -6,23 +15,71 @@ class Robot:
     # TODO 这个配置可以写入到配置文件中
     ROBOT_UNLOAD_PERIOD = 3600
 
-    def __init__(self, rbtID=None, simIdx=None):
-        # 这里只定义机器人服务中必须用到的属性，其它信息可在detail函数中获取
+    def __init__(self):
         # 机器人ID
         self.__rbt_id = None
-        # 知识库，用于文本比较
-        self.__knowledge = {}
+        # 语料库，用于文本比较，use_model=0
+        self.__corpus = {}
         # 模型，用于模型判断
         self.__model = None
-        # 相似度法制
-        self.__sim_idx = 0.9
+        # 模型判断之后，再使用相似度计算出模型判断的准确值，use_model=0,1
+        self.__knowledge = {}
+        # 相似度阀值
+        self.__sim_idx = 0.0
         # 自动卸载时间
+        self.__unloaded_at = None
+        # 公司ID
+        self.__company_id = None
+        # 公司账号
+        self.__company_account = None
+        # 企业机器人所属行业的行业机器人ID（允许为空）
+        self.__industry_robot_id = None
+
+    def assemble(self, rbt_id):
+        """
+        根据rbt_id组装机器人服务对象
+        组装内容包括
+        __rbt_id: 机器人ID，直接赋值即可
+        __sim_idx: 相似度的阀值，可以从机器人表里面拿，如果没有设置，就从配置文件里面拿默认值
+        __knowledge: 用来做前置判断，文本相似度的
+        __model: 模型
+        __model_knowledge: 全部的知识库，用来给模型判断加精准度指数
+        __industry_robot_id：企业机器人所属行业的行业机器人ID
+        1、设置rbt_id
+        2、根据rbt_id查询机器人信息：rbt_robot表、rbt_industry表联合查询
+        3、根据industry_id字段，查询是否有企业机器人ID
+        3.1 如果industry_id=None，
+        4、
+        :param rbt_id:
+        :return:
+        """
+        self.__rbt_id = rbt_id
+
+        # 设置自动卸载时间
         self.__unloaded_at = getRobotUnloadTime(Robot.ROBOT_UNLOAD_PERIOD)
 
-        if rbtID is not None:
-            self.__rbt_id = rbtID
-        if simIdx is not None:
-            self.__sim_idx = simIdx
+        sql = '''select robot.rbt_id, robot.company_id, robot.company_account, robot.sim_idx,industry.robot_id industry_robot_id from ai_chatrobot.rbt_robot robot left join ai_chatrobot.rbt_industry industry on robot.industry_id=industry.id where robot.rbt_id=:rbt_id and status=:status and type=:type '''
+        params = {'rbt_id': rbt_id, 'status': RobotConstants.RBT_STATUS_ON, 'type': RobotConstants.RBT_TYPE_COMPANY}
+        queryData = queryBySQL(app=current_app, sql=sql, params=params)
+        if queryData.__len__() == 1:
+            data = queryData[0]
+            self.__company_id = data.get('company_id')
+            self.__company_account = data.get('company_account')
+            if data.get('company_account') is None:
+                self.__sim_idx = float(data.get('company_account'))
+            else:
+                # TODO 这里后期替换为配置文件的值
+                self.__sim_idx = 0.8
+            self.__industry_robot_id = data.get('industry_robot_id')
+
+            self.__corpus, self.__knowledge = getKnowledgeDataForAnswer(app=current_app, rbt_id=rbt_id)
+
+            assemble_model(self, rbt_id)
+        else:
+            # 理论上应该只有一条记录，无记录或是多条记录都是错误的
+            errMsg = "机器人{}信息查询失败！".format(rbt_id)
+            raise Exception(errMsg)
+        pass
 
     def __str__(self):
         return self.detail(self)
