f2bf3bc70f54c26c8087df90f837b327f99dee25
chengxb <chengxb@53kf.com>
2021-05-14 13:02:18 +0800

    分析各个行业的知识库数据功能完成并通过测试环境测试。

--- scripts/initial_industry_knowledge_lib/initial_industry_knowledge_lib.py
+++ scripts/initial_industry_knowledge_lib/initial_industry_knowledge_lib.py
@@ -1,21 +1,3 @@
-"""
-本文件是用于
-1、许栋栋根据行业ID（二级行业），从许栋栋处获取该行业下所有企业的行业知识库
-把标准问题和答案存储到cloud_customer_service.rbt_industry_knowledge_lib_init_data表
-2、使用聚类分析功能，对表里的数据进行分析，
-2.1 接受industry_id作为读入参数，检查数据是否存在，不存在就结束
-2.2 设置数据的status=0（重复运行）
-2.3 开始进行分析，分级结果以id作为分组依据
-2.4 分析完成之后，同组的ID取名：分组1，分组2，以此类推
-2.5 分组名更新数据表
-2.6 行业数据运行完成之后，这个行业下的数据设置status=1（分析完成）
-3、分析结果数据会显示到saas管理平台：
-【机器人行业库】-【初始行业库】
-4、界面上进行【处理】操作：把数据添加到行业库
-处理之后，把这个数据的status=2
-
-行业机器人需要返回实体ID（分组、标签都是一个概念）
-"""
 import os
 import sys
 
@@ -25,11 +7,8 @@ from common.log import Log
 from common.textSimilarity import CosSim
 from common.utils import *
 
-# 当前目录
 basePath = os.path.abspath(os.path.dirname(__file__))
-# 设置当前目录为执行运行目录
 sys.path.append(basePath)
-# 获取文件名（含后缀）
 currentFileName = os.path.basename(__file__)
 
 SIM_IDX = 0.6
@@ -92,9 +71,6 @@ def updateIndustryStatus(industry_id):
 
 
 log.info("程序开始...")
-# 1、获得尚未处理的行业ID
-sql = '''update rbt_industry_knowledge_lib_init_data set `group`=null, status=0'''
-executeBySQL(conn=conn, sql=sql)
 
 sql = '''select distinct(industry_id) industry_id from cloud_customer_service.rbt_industry_knowledge_lib_init_data where status=0 order by industry_id asc'''
 industry_ids = queryBySQL(conn=conn, sql=sql)
@@ -123,7 +99,6 @@ for industry_data in industry_ids:
                 if compareItems(baseItem, item):
                     group.append(item)
             updateGroupData(group)
-            # 去掉已经处理过的数据
             id_sentence_array = [item for item in id_sentence_array if item not in group]
             log.info("剩余数据量：{}".format(id_sentence_array.__len__()))
 
